d43aa10d058378d284d7255e64cf939b9db80739
erick <a22006182@alunos.ulht.pt>
2023-09-18 23:30:48 +0100

    Read More style fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,14 +13,11 @@ export const Card = ({movie}) => {
 			</Link>
 			<div className="p-5">
 				<Link to={`/movie/${id}`}>
-					<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{original_title}</h5>
+					<h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{original_title}</h1>
 				</Link>
-				<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
-				<Link to={`/movie/${id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+				<p className="mb-3 pb-8 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
+				<Link to={`/movie/${id}`} className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">
 					Read more
-					<svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-						<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-					</svg>
 				</Link>
 			</div>
 		</div>
