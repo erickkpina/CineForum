29f410ffaa45085f97d36474c724fda6db08266c
erick <a22006182@alunos.ulht.pt>
2023-09-18 22:57:31 +0100

    Minor Fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -38,7 +38,7 @@ export const Header = () => {
 			<nav className="bg-white border-b-2 border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900 dark:border-b-1 dark:border-gray-900">
 				<div className="flex flex-wrap justify-between items-center mx-auto">
 					<Link to="/" className="flex items-center">
-						<img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Cineforum Logo" />
+						<img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-8 sm:h-9" alt="Cineforum Logo" />
 						<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Cineforum</span>
 					</Link>
 
